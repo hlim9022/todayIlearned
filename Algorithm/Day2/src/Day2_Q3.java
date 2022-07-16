96e4e23e0dbac878093bf6fd67b9d3420eabc132
Heylim Kim <hlim9022@gmail.com>
2022-07-16 13:51:45 +0900

    int 형변환 오류 -> long으로 casting해서 수정완료

--- Algorithm/Day2/src/Day2_Q3.java
+++ Algorithm/Day2/src/Day2_Q3.java
@@ -1,11 +1,13 @@
 public class Day2_Q3 {
-    public int solution(int num) {
+    public static int solution(int num) {
         int count = 0;
+        long n = (long) num;
 
-        while (!(num == 1 || count == 500)) {
-            if (num % 2 == 0) num /= 2;
-            else if (num % 2 == 1) num = num * 3 + 1;
+        while (!(n == 1 || count == 500)) {
+            if (n % 2 == 0) n /= 2;
+            else n = n * 3 + 1;
 
+            System.out.println(n);
             count++;
         }
         return (count == 500)? -1 : count;
