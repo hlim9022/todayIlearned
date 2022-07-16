da33e7257fa0229aee855c55ac14df861167a7ec
Heylim Kim <hlim9022@gmail.com>
2022-07-16 14:26:19 +0900

    알고리즘풀이_Day2_Java(Q3 코드 정리)

--- Algorithm/Day2/src/Day2_Q3.java
+++ Algorithm/Day2/src/Day2_Q3.java
@@ -1,5 +1,5 @@
 public class Day2_Q3 {
-    public static int solution(int num) {
+    public int solution(int num) {
         int count = 0;
         long n = (long) num;
 
@@ -7,7 +7,6 @@ public class Day2_Q3 {
             if (n % 2 == 0) n /= 2;
             else n = n * 3 + 1;
 
-            System.out.println(n);
             count++;
         }
         return (count == 500) ? -1 : count;
