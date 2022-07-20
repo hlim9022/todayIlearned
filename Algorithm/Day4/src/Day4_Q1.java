46eeca45a0f80651af35408d1b1da64843e65f78
Heylim Kim <hlim9022@gmail.com>
2022-07-20 20:47:47 +0900

    Revert "알고리즘풀이_Day4_Java"
    This reverts commit b2abe0bc887ac227f7334b788091b9d57ffa4402.

--- Algorithm/Day4/src/Day4_Q1.java
+++
@@ -1,8 +0,0 @@
-public class Day4_Q1 {
-    public int solution(int left, int right) {
-        int sum = 0;
-        for (int i = left; i <= right; i++)
-            sum += (i % Math.sqrt(i) == 0)? -i : i;
-        return sum;
-    }
-}
